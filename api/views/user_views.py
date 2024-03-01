06eee828c7b4c4ce56684454533eaff02e66f977
Zakhy <lirchzakhar@gmail.com>
2024-03-01 21:08:53 +0700

    delete unused import

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -1,6 +1,5 @@
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from api.serializers.user_serializers import UserSerializerCreate, AuthTokenSerializer
