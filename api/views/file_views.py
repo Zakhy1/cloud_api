b301d6a11ae43390d45a308595d33ed9c2c26705
Zakhy <lirchzakhar@gmail.com>
2024-03-03 16:13:10 +0700

    cleaning

--- api/views/file_views.py
+++ api/views/file_views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework import status, viewsets
 
 from api.models import File, User, Access
-from cloud_api.generics.common import response_error
+from cloud_api.generics.common import response_error, response_success
 from cloud_api.generics.permissions import CustomIsOwner
 from api.serializers.file_serializers import FileSerializer, UploadedFileSerializer, FileWithAccessSerializer, \
     AccessSerializer
@@ -39,7 +39,7 @@ class FilesViewSet(viewsets.ModelViewSet):
                                                 file=file)
             Access.objects.create(user=request.user, file=uploaded_file, author=True)
             host = 'http://127.0.0.1:8000'
-            uploaded_files.append({  # TODO serializer
+            uploaded_files.append({  # TODO serializer type = serializers.ReadOnlyField(default='question')
                 'success': True,
                 'message': 'Success',
                 'name': uploaded_file.name,
@@ -49,33 +49,27 @@ class FilesViewSet(viewsets.ModelViewSet):
             return Response(uploaded_files, status=status.HTTP_201_CREATED)
         return response_error(serializer.errors)
 
-    def retrieve(self, request, *args, **kwargs):
+    def retrieve(self, request, **kwargs):
         file = get_object_or_404(File, pk=kwargs['pk'])
         if file.owner == request.user:  # TODO check without
             return FileResponse(open(file.file.path, 'rb'))
         raise PermissionDenied(detail='Forbidden for you', code=403)
 
-    def update(self, request, *args, **kwargs):
+    def update(self, request, **kwargs):
         file = get_object_or_404(File, pk=kwargs['pk'])
         file.name = request.data['name']
         file.save()
         if file.owner == request.user:
-            return Response({
-                'success': True,
-                'message': 'Renamed'
-            })
+            return response_success('Renamed')
         raise PermissionDenied(detail='Forbidden for you', code=403)
 
     def destroy(self, request, *args, **kwargs):
         file = get_object_or_404(File, pk=kwargs['pk'])
         file.delete()
-        return Response({
-            'success': True,
-            'message': 'File already deleted',
-        })
+        return response_success('File already deleted')
 
     @action(methods=('post', 'delete'), detail=True, url_path='accesses')
-    def manage_access(self, request, *args, **kwargs):
+    def manage_access(self, request, **kwargs):
 
         try:
             user = User.objects.get(email=request.data['email'])
@@ -98,15 +92,15 @@ class FilesViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
 
     @action(methods=('get',), detail=False, url_path='shared')
-    def get_shared_files(self, request, *args, **kwargs):
+    def get_shared_files(self):
         user = self.request.user
         queryset = File.objects.filter(accesses__user_id=user).exclude(owner=user)
         serializer = UploadedFileSerializer(queryset, many=True)
         return Response(serializer.data)
 
     @action(methods=('get',), detail=False, url_path='disk')
-    def get_all_files(self, request, *args, **kwargs):
+    def get_all_files(self):
         user = self.request.user
         files = File.objects.filter(owner=user)
         file_serializer = FileWithAccessSerializer(files, many=True)
-        return Response(file_serializer.data, )
+        return Response(file_serializer.data)
